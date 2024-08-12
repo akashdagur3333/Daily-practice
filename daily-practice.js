58540b4fa23897da2db03d76b54d960c615406ea
akashdagur3333 <akashdagur3333@gmail.com>
2024-08-13 01:09:59 +0530

    Update daily-practice.js

--- daily-practice.js
+++ daily-practice.js
@@ -85,12 +85,242 @@ console.log('let start with daily practice')
 // 5  How to find the majority element in an array (if it exists)?
 
 
-// function findMajorityElement(arr) {
+// function findMajorityElement(nums) {
+//     let candidate = null;
+//     let count = 0;
+//     // Step 1: Find the candidate
+//     for (let num of nums) {
+//         if (count === 0) {
+//             candidate = num;
+//             count = 1;
+//         } else if (num === candidate) {
+//             count++;
+//         } else {
+//             count--;
+//         }
+
+
+//     }
+//     count = 0;
+//     for (let num of nums) {
+//         if (num === candidate) {
+//             count++;
+//         }
+//     }
+//     return count > Math.floor(nums.length / 2) ? candidate : null;
 // }
 
-// // Test the function
+
+// function findMajorityElement(arr){
+//     let count=0;
+//     let compare=null;
+// for(let a of arr){
+//     if(count==0){
+//         compare=a;
+//         count=1
+//     }
+//     else if (compare==a){
+//         count++
+//     }
+//     else{
+//         count--
+//     }
+// }
+
+// count=0
+// for(let b of arr){
+//     if(compare==b){
+//         count++
+//     }
+// }
+
+// return count>Math.floor(arr.length/2)?compare:null
+// }
+
+
+// // // Test the function
 // const arr1 = [3, 3, 4, 2, 4, 4, 2, 4, 4];
 // console.log(findMajorityElement(arr1)); // Output: 4
 
 // const arr2 = [3, 3, 4, 2, 4, 4, 2, 4];
 // console.log(findMajorityElement(arr2)); // Output: null
+
+
+
+// rotateArray  const nums = [1, 2, 3, 4, 5, 6, 7];
+
+
+// const rotateArray=(arr)=>{
+// let left=0;
+// let right=arr.length-1
+// let temp=0;
+// while(left<right){
+//     temp=arr[left]
+//     arr[left]=arr[right]
+//     arr[right]=temp
+//     left++;
+//     right--
+// }
+// }
+
+// const nums =[3, 3, 4, 2, 4, 4, 2, 4, 4];
+// rotateArray(nums)
+
+
+// function rotateArray(arr,k){
+//     let start=0;
+//    for(let i=arr.length-1;i>=0;i--){
+//       let temp=arr[i];
+//       arr[start]=temp
+//     console.log(arr[i])
+//    }
+// }
+
+// const nums = [1, 2, 3, 4, 5, 6, 7];
+// const k = 3;
+// rotateArray(nums, k);
+
+// function intersection(arr1,arr2){
+//  const check=new Set(arr1)
+//  const result=new Set()
+//  for(let a of arr2){
+//     if(check.has(a)){
+//     result.add(a)
+//     }
+//  }
+//  return Array.from(result)
+// }
+
+// // const nums1 = [4, 9, 5];
+// // const nums2 = [9, 4, 9, 8, 4];
+// // const nums1 = [1, 2, 3];
+// // const nums2 = [4, 5, 6];
+
+// // const nums1 = [7, 7, 8, 8];
+// // const nums2 = [8, 8, 9, 9];
+
+// // const nums1 = [10, 20, 30, 40, 50];
+// // const nums2 = [40, 50, 60, 70, 80];
+
+// const nums1 = [15, 25, 35, 45, 55];
+// const nums2 = [65, 75, 85, 95];
+
+// const result = intersection(nums1, nums2);
+// console.log(result);  // Output: [9, 4]
+
+
+// const nonZeroes=(arr)=>{
+// let realPos=0
+//    for(let i =0;i<arr.length;i++){
+//     if(arr[i]!=0){
+//         if(realPos!=i){
+//             [arr[realPos],arr[i]]=[arr[i],arr[realPos]]
+//         }
+//       realPos++
+//       console.log(arr)
+
+//     }
+//    }
+//    return arr
+// //   console.log(arr)
+
+// }
+// let nums = [0, 1, 0, 3, 12]
+
+// console.log(nonZeroes(nums))
+
+// function sum(num){
+//   var result = 0;
+//   for(var i = 0; i <= num.length; i++){
+//     result += num[i]
+//   }
+//   return result;
+// }
+
+// var numbers = [1,2,3,4,5];
+// console.log(sum(numbers));
+
+
+// const isPalindrome =(name)=>{
+//     name=name.toLowerCase().replace(/[^a-z0-9]/g, '');
+//     let left=0
+//     let right=name.length-1
+//     while(left<right){
+//         if(name[left]!=name[right]){
+//            return false;
+//         }
+//         left++;
+//         right--;
+//     }
+//    return true;
+// }
+
+
+// // console.log(isPalindrome("raCecar")); // Output: true
+// // console.log(isPalindrome("hello"));   // Output: false
+// console.log(isPalindrome("A man, a plan, a canal, Panama")); // Output: true
+
+// const countFrequency=(s)=>{
+//     const result={}
+// for(let a of s){
+//   result[a]=(result[a] || 0)+1
+// }
+// return result
+// }
+
+// let name="hello";
+// // console.log(countFrequency(name))
+// console.log(countFrequency("mississippi")); 
+
+
+// // How to reverse a string?
+// const reverseString = (s) => {
+//     // Convert the string to an array
+//     let arr = s.split('');
+
+//     // Two-pointer approach
+//     let left = 0;
+//     let right = arr.length - 1;
+    
+//     while (left < right) {
+//         // Swap the characters
+//         let temp = arr[left];
+//         arr[left] = arr[right];
+//         arr[right] = temp;
+
+//         // Move pointers towards the center
+//         left++;
+//         right--;
+//     }
+
+//     // Join the array back into a string and return it
+//     return arr.join('');
+// };
+
+// // Example usage
+// console.log(reverseString("hello"));   // Output: "olleh"
+
+
+
+
+// let name="hello";
+// console.log(reverseString(name))
+
+
+// const areAnagrams=(str1,str2)=>{
+//     const freq={}
+//     for(let a of str1){
+//         freq[a]=(freq[a] || 0)+1
+//     }
+//     for(let b of str2){
+//         if(!freq[b]){
+//             return false
+//         }
+//         freq[b]--;
+//     }
+//     return true
+// }
+
+// console.log(areAnagrams("listen", "silent"));  // Output: true
+// console.log(areAnagrams("hello", "oellh"));    // Output: true
+// console.log(areAnagrams("hello", "world")); // false
